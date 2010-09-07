460d59ea46f4bda6bf11a6d83239324345ab7bc8
danil.korotenko <danil.korotenko@e34b3dd6-a7c9-c8d4-14fe-c837c0849a06>
2010-09-07 19:54:00 +0000

    Application now has a tray icon menu.
    git-svn-id: https://uptimer.googlecode.com/svn/trunk@5 e34b3dd6-a7c9-c8d4-14fe-c837c0849a06

--- src/uapplication.cpp
+++ src/uapplication.cpp
@@ -5,9 +5,22 @@ UApplication::UApplication(int argc, char *argv[]) : QApplication(argc, argv)
 	this->configureTrayIcon();
 }
 
+UApplication::~UApplication()
+{
+	delete quitAction;
+	delete trayMenu;
+}
+
 //Private methods
 void UApplication::configureTrayIcon()
 {
+	quitAction = new QAction(tr("&Quit"), this);
+	connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+	trayMenu = new QMenu();
+	trayMenu->addAction(quitAction);
+
 	trayIcon.setIcon(QIcon(":/icons/Clock.ico"));
+	trayIcon.setContextMenu(trayMenu);
 	trayIcon.show();
 }
--- src/uapplication.h
+++ src/uapplication.h
@@ -3,12 +3,15 @@
 
 #include <QApplication>
 #include <QSystemTrayIcon>
+#include <QAction>
+#include <QMenu>
 
 class UApplication : public QApplication
 {
 	Q_OBJECT
 public:
 	UApplication(int argc, char *argv[]);
+	~UApplication();
 
 signals:
 
@@ -17,6 +20,10 @@ public slots:
 private:
 //Data
 	QSystemTrayIcon trayIcon;
+	QMenu *trayMenu;
+
+//Actions
+	QAction *quitAction;
 
 //Methods
 	void configureTrayIcon();
